5d7d9b93c1527e466f2c576af502c85bc5ea067b
Greg Mefford <greg@gregmefford.com>
2018-01-06 14:43:28 -0500

    Test set_pixel with a matrix

--- test/neopixel_test.exs
+++ test/neopixel_test.exs
@@ -1,4 +1,6 @@
 defmodule Nerves.Neopixel.NeopixelTest do
+  # We have to use async: false for these because the HAL wants to register its
+  # name globally, so we can only run one at a time.
   use ExUnit.Case, async: false
 
   doctest Nerves.Neopixel
@@ -6,37 +8,102 @@ defmodule Nerves.Neopixel.NeopixelTest do
   alias Nerves.Neopixel
   alias Neopixel.HAL
 
-  defp with_neopixel_stick(_) do
+  defp with_neopixel_stick_and_unicorn_phat(_) do
     Application.stop(:nerves_neopixel)
-    {:ok, _pid} = HAL.start_link(config: neopixel_stick_config())
+    {:ok, _pid} = HAL.start_link(config: neopixel_stick_and_unicorn_phat_config())
+    GenServer.call(HAL, :subscribe)
+    flush()
     :ok
   end
 
   describe "Nerves.Neopixel.set_pixel" do
-    setup [:with_neopixel_stick]
+    setup [:with_neopixel_stick_and_unicorn_phat]
+    # Arrangement looks like this:
+    # Y  X: 0  1  2  3  4  5  6  7
+    # 0  [  0  1  2  3  4  5  6  7 ] <- Adafruit NeoPixel Stick on Channel 1 (offset 0)
+    #    |-------------------------|
+    # 1  |  0  1  2  3  4  5  6  7 |
+    # 2  |  8  9 10 11 12 13 14 15 | <- Pimoroni Unicorn pHat on Channel 2 (offset 1)
+    # 3  | 16 17 18 19 20 21 22 23 |
+    # 4  | 24 25 26 27 28 29 30 31 |
+    #    |-------------------------|
 
     test "it works with RGB colors" do
-      GenServer.call(HAL, :subscribe)
-      Neopixel.set_pixel({1, 0}, {255, 0, 255})
+      Neopixel.set_pixel({0, 0}, {255, 0, 128})
       Neopixel.render()
-      assert_receive "Called set_pixel(x: 1, y: 0, color: 0x00ff00ff)"
+      assert_receive "Called set_pixel(x: 0, y: 0, color: 0x00ff0080)"
       assert_receive "Called render()"
-      assert_receive "  [0][1]: 0x00ff00ff"
+      assert_receive "  [0][0]: 0x00ff0080"
+    end
+
+    test "it works with RGBW colors" do
+      Neopixel.set_pixel({0, 0}, {255, 0, 128, 64})
+      Neopixel.render()
+      assert_receive "Called set_pixel(x: 0, y: 0, color: 0x40ff0080)"
+      assert_receive "Called render()"
+      assert_receive "  [0][0]: 0x40ff0080"
+    end
+
+    test "it renders the pixel in the correct location on a strip" do
+      Neopixel.set_pixel({6, 0}, {255, 0, 128, 64})
+      Neopixel.render()
+      assert_receive "Called set_pixel(x: 6, y: 0, color: 0x40ff0080)"
+      assert_receive "Called render()"
+      assert_receive "  [0][6]: 0x40ff0080"
+    end
+
+    test "it renders the pixel in the correct location on a matrix" do
+      Neopixel.set_pixel({6, 3}, {255, 0, 128, 64})
+      Neopixel.render()
+      assert_receive "Called set_pixel(x: 6, y: 3, color: 0x40ff0080)"
+      assert_receive "Called render()"
+      assert_receive "  [1][22]: 0x40ff0080"
+    end
+
+  end
+
+  defp flush(type \\ :silent, opts \\ [])
+  defp flush(:silent, opts) do
+    receive do
+      _msg -> flush(:silent, opts)
+    after
+      0 -> :ok
+    end
+  end
+  defp flush(:inspect, opts) do
+    receive do
+      msg ->
+        IO.inspect(msg, opts)
+        flush(:inspect, opts)
+    after
+      0 -> :ok
     end
   end
 
-  defp neopixel_stick_config do
+  defp neopixel_stick_and_unicorn_phat_config do
     [
-      canvas: {8, 1},
-      channels: [:channel1],
+      canvas: {8, 5},
+      channels: [:channel1, :channel2],
       channel1: [
-        pin: 18,
+        pin: 13,
         arrangement: [
           %{
             type: :strip,
             origin: {0, 0},
             count: 8,
             direction: :right
+          }
+        ]
+      ],
+      channel2: [
+        pin: 18,
+        arrangement: [
+          %{
+            type: :matrix,
+            origin: {0, 1},
+            count: {8, 4},
+            direction: {:right, :down},
+            progressive: true
           }
         ]
       ]
