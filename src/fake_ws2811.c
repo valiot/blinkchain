b39ce75d9ac5bf844393f20b6c8887d1e8dfa538
Greg Mefford <greg@gregmefford.com>
2018-02-26 22:15:36 -0500

    run `astyle` on C code

--- src/fake_ws2811.c
+++ src/fake_ws2811.c
@@ -11,25 +11,25 @@
 #define RPI_PWM_CHANNELS 2
 
 ws2811_return_t ws2811_init(ws2811_t *ws2811) {
-  int chan;
-  for (chan = 0; chan < RPI_PWM_CHANNELS; chan++) {
-    ws2811_channel_t *channel = &ws2811->channel[chan];
-    channel->leds = calloc(channel->count, sizeof(ws2811_led_t));
-    channel->strip_type=WS2811_STRIP_RGB;
-  }
-  return WS2811_SUCCESS;
+    int chan;
+    for (chan = 0; chan < RPI_PWM_CHANNELS; chan++) {
+        ws2811_channel_t *channel = &ws2811->channel[chan];
+        channel->leds = calloc(channel->count, sizeof(ws2811_led_t));
+        channel->strip_type=WS2811_STRIP_RGB;
+    }
+    return WS2811_SUCCESS;
 }
 
 ws2811_return_t ws2811_render(ws2811_t *ws2811) {
-  debug("Called render()");
-  uint8_t ch;
-  uint16_t offset;
-  for(ch = 0; ch < RPI_PWM_CHANNELS; ch++) {
-    for(offset = 0; offset < ws2811->channel[ch].count; offset++) {
-      debug("  [%hhu][%hu]: 0x%08x", ch, offset, ws2811->channel[ch].leds[offset]);
+    debug("Called render()");
+    uint8_t ch;
+    uint16_t offset;
+    for(ch = 0; ch < RPI_PWM_CHANNELS; ch++) {
+        for(offset = 0; offset < ws2811->channel[ch].count; offset++) {
+            debug("  [%hhu][%hu]: 0x%08x", ch, offset, ws2811->channel[ch].leds[offset]);
+        }
     }
-  }
-  return WS2811_SUCCESS;
+    return WS2811_SUCCESS;
 }
 
 const char * ws2811_get_return_t_str(const ws2811_return_t state) {
